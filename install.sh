7949e849b70ac7f1751b8eeffa76b76ee131aaf9
Bill Butler <bill@butler.net>
2015-06-10 13:12:00 -0500

    Change the mv command

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ if [ ! -d vendor/jsPDF ]; then
     wget --no-check-certificate -O jsPDF.zip https://github.com/MrRio/jsPDF/archive/v1.0.272.zip
     unzip jsPDF.zip
     mkdir vendor
-    mv jsPDF-master vendor/jsPDF
+    mv jsPDF-1.0.272 vendor/jsPDF
     rm jsPDF.zip
 fi
